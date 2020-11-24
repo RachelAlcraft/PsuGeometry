6f6fa36bafb21c75657c7d089f5d76de226a006b
Rachel A <rachelalcraft@gmail.com>
2020-11-24 13:58:15 +0000

    Update test_slice_only.py

--- PsuGeometry/tests/test_slice_only.py
+++ PsuGeometry/tests/test_slice_only.py
@@ -1,9 +1,10 @@
 from PsuGeometry import GeoSlice as gsl
-printPath = '/home/rachel/Documents/Bioinformatics/ProteinDataFiles/results_psu/slices/'
+#printPath = '/home/rachel/Documents/Bioinformatics/ProteinDataFiles/results_psu/slices/'
+printPath = 'F:/Code/ProteinDataFiles/ccp4_data/'
 
 gs = gsl.GeoSlice()
-slice = gs.load(printPath + "/slice.csv")
+slice = gs.load(printPath + "1ejg_diff.ccp4_slice.csv")
 
 gs.addDensitySlice(slice,palette='cubehelix_r')
 
-gs.printToHtml('Test slice only',4,printPath + "/sliceonly.html")
+gs.printToHtml('Test slice only',4,printPath + '1ejg_diff.ccp4_slice.html')
