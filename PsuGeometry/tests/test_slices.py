c94101dd18b23755869a68597f3735f12bd8ab30
RachelAlcraft <rachelalcraft@gmail.com>
2020-12-11 14:07:39 +0000

    Slices report

--- PsuGeometry/tests/test_slices.py
+++ PsuGeometry/tests/test_slices.py
@@ -9,28 +9,34 @@ pdbDataPath = '/home/rachel/Documents/Bioinformatics/ProteinDataFiles/pdb_data/'
 edDataPath = '/home/rachel/Documents/Bioinformatics/ProteinDataFiles/ccp4_data/'
 printPath = '/home/rachel/Documents/Bioinformatics/ProteinDataFiles/results_psu/slicesgeo/'
 
-filenames = ['Slices/1ejg/1TYR-1ejg.ccp4_slice.csv','Slices/1ejg/2TYR-1ejg.ccp4_slice.csv']
-filenamesdiffs = ['Slices/1ejg/1TYR-1ejg_diff.ccp4_slice.csv','Slices/1ejg/2TYR-1ejg_diff.ccp4_slice.csv']
+dir = 'Slices/1us0/'
+fileroot = '1us0.ccp4_slice.csv'
+filerootdiff = '1us0_diff.ccp4_slice.csv'
+residues = ['A39','A48','A82','A102','A107','A177','A189','A198','A209','A291','A309']
+pdb = '1us0'
+
+
+
 georep = psu.GeoReport([],pdbDataPath,edDataPath,printPath,ed=True,dssp=True)
 
 slices = []
 slicesdiffs = []
 
-for fn in range(0,len(filenames)):
-    slice = georep.loadSlice(filenames[fn])
-    slicediff = georep.loadSlice(filenamesdiffs[fn])
+for res in residues:
+    slice = georep.loadSlice(dir+res+fileroot)
+    slicediff = georep.loadSlice(dir+res+filerootdiff)
     slices.append(slice)
     slicesdiffs.append(slicediff)
 
-    georep.addSlice(slice, palette='cubehelix_r')
-    georep.addSlice(slice, palette='cubehelix_r', logged=True)
-    georep.addSlice(slicediff, palette='seismic', logged=False,centre=True)
+    georep.addSlice(slice, palette='cubehelix_r',title=res)
+    georep.addSlice(slice, palette='cubehelix_r', logged=True, title=res + " logged")
+    georep.addSlice(slicediff, palette='seismic', logged=False,centre=True, title= res + ' difference')
 
 
 
 georep.addSlices(slices, palette='cubehelix_r', title='Average', logged=False, centre=False)
-georep.addSlices(slices, palette='cubehelix_r', title='Average', logged=True, centre=False)
-georep.addSlices(slicesdiffs, palette='seismic', title='Average', logged=False, centre=True)
+georep.addSlices(slices, palette='cubehelix_r', title='Average logged', logged=True, centre=False)
+georep.addSlices(slicesdiffs, palette='seismic', title='Average difference', logged=False, centre=True)
 
 
-georep.printToHtml('Slices from Density Flight', 3, 'ccp4_slices')
+georep.printToHtml('Slices from Density Flight TYR for 1US0', 3, 'ccp4_slices' + pdb)
