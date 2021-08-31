59192582b2a5eac3730e24c0ab3a2744147a8fe0
RachelAlcraft <RachelAlcraft@gmail.com>
2021-08-31 15:19:32 +0100

    Updated dssp

---
+++ PsuGeometry/Chapters/Chapter001/Ch000_001_csv1_dssp_LINUX.py
@@ -0,0 +1,16 @@
+
+import pandas as pd
+import Ch000_Functions as help
+from PsuGeometry import GeoReport as psu
+pdblist = help.getPDBList100()
+pdblist.sort()
+#pdblist = pdblist[:10]
+hueList = ['aa', 'rid', 'bfactor', 'pdbCode', 'bfactorRatio', 'disordered','occupancy','dssp']
+
+dsspPrintPath = '../../PdbLists/'
+
+georep = psu.GeoReport(pdblist, help.pdbDataPathLx, help.edDataPath, dsspPrintPath, ed=False, dssp=True, includePdbs=False, keepDisordered=True)
+datacsv = georep.getGeoemtryCsv(['N:CA'],hueList)
+datacsv = datacsv[['pdbCode','chain','rid','aa','dssp']]
+print(datacsv)
+datacsv.to_csv(dsspPrintPath + 'dssp.csv', index=False)
